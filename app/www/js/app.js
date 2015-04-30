d65d5dc6dc1e3b32b881b7ffa8c0b144ec8628e3
Leo Mylonas <leo@mylonas.co>
2015-04-30 22:56:51 +1000

    Added plugins. Created most of the bluetooth code. Doesn't seem to want to connect to my Win8 PC

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -1,19 +1,127 @@
 // Ionic Starter App
 
+var ionicNavBarDelegate = null;
+
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+.config(function($ionicConfigProvider) {
+    $ionicConfigProvider.navBar.alignTitle('ios');
 })
+
+.run(function($ionicPlatform, $rootScope, $ionicNavBarDelegate) {
+    $ionicPlatform.ready(function() {
+
+        // get a global copy of the nav bar delegate
+        ionicNavBarDelegate = $ionicNavBarDelegate;
+
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard for form inputs)
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+
+        // set the style of the status bar
+        if(window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+
+        // check what platform we're running on
+        if (typeof device != "undefined") {
+            device.isAndroid = (device.platform.toLowerCase() == "android");
+            device.isIos = (device.platform.toLowerCase() == "ios");
+        } else {
+            // we're running on an unsupported platform (eg browser)
+            window.device = {
+                isAndroid: false,
+                isIos: false
+            }
+        }
+
+        $rootScope.messages = [];
+
+        // start the bluetooth communication
+        Bluetooth.start();
+        Bluetooth.onDevicesFoundHandler = function(devices) {
+            // LEO-PC   E0:B9:A5:9D:AE:81
+            //Bluetooth.connect("E0:B9:A5:9D:AE:81");
+        }
+    });
+})
+
+var Bluetooth = {
+    onReceivedDataHandler: null,
+    onDevicesFoundHandler: null,
+
+    start: function() {
+        Log("Starting the Bluetooth service");
+        bluetoothSerial.isEnabled(function() {
+            Bluetooth.searchDevices();
+        }, function() {
+            console.log("Bluetooth is not enabled");
+            if (device.isAndroid) {
+                console.log("Enabling Bluetooth");
+                bluetoothSerial.enable(Bluetooth.searchDevices);
+            } else if (device.isIos) {
+                alert("Please enable Bluetooth!");
+            }
+        });
+    },
+
+    searchDevices: function() {
+        Log("Searching for Bluetooth devices");
+        if (device.isAndroid) {
+            bluetoothSerial.discoverUnpaired(Bluetooth.onDevicesFound, Error);
+        } else if (device.isIos) {
+            bluetoothSerial.list(Bluetooth.onDevicesFound, Error);
+        }
+    },
+
+    onDevicesFound: function(devices) {
+        if (devices.length == 0) {
+            Log("No Bluetooth devices found");
+        } else {
+            Log("Found Bluetooth devices", devices);
+            if (typeof Bluetooth.onDevicesFoundHandler == "function") {
+                Bluetooth.onDevicesFoundHandler();
+            }
+        }
+    },
+
+    connect: function(address) {
+        Log("Connecting to", address);
+        bluetoothSerial.connect(address, function() {
+            console.log("Successfully connected to", address);
+            bluetoothSerial.subscribe('\n', Bluetooth.onReceivedData, Error);
+        }, Error);
+    },
+
+    onReceivedData: function(data) {
+        Log("Received data", data);
+        if (typeof Bluetooth.onReceivedDataHandler == "function") {
+            Bluetooth.onReceivedDataHandler();
+        }
+    }
+}
+
+function Log() {
+    if (typeof arguments[0] == "string") {
+        ionicNavBarDelegate.title(arguments[0]);
+    }
+    var log = "console.log(";
+    for (var i=0; i<arguments.length; i++) {
+        log += "arguments["+i+"], ";
+    }
+    log = log.substring(0, log.length-2) + ")";
+    eval(log);
+}
+
+function Error() {
+    var error = "console.error(";
+    for (var i=0; i<arguments.length; i++) {
+        error += "arguments["+i+"], ";
+    }
+    error = error.substring(0, error.length-2) + ")";
+    eval(error);
+}
