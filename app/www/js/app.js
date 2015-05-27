bd96f99776ccc1399d2527076f44e414702da21d
Kelly Baxendale <kbaxenda@gmail.com>
2015-05-27 10:09:28 +1000

    Working on Tap detection

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -50,15 +50,53 @@ var Doublestep = angular.module('Doublestep', ['ionic'])
 	$urlRouterProvider.otherwise('/ble');
 })
 
+
 .controller('BleCtrl', function($scope, $ionicPlatform) {
 	$scope.messages = [];
+	var readings = [];
+	var rangeHigh = 1024;
+	var rangeLow = 0;
+	var numReadings = 50;
+	var buffer = 100; //amount to add to range
+	var checkTap = false;
 
     $ionicPlatform.ready(function() {
         // start the bluetooth communication
     	Bluetooth.start();
     	Bluetooth.onReceivedDataHandler = function(value) {
-    		$scope.messages.push(value);
-    		$scope.$apply();
+		if(checkTap) {
+			if(value > rangeHigh) {
+				$scope.messages.push("Tap");
+				$scope.apply();
+				//Didn't add this one to readings array
+			}
+			checkTap = false;
+		} else {
+		if(readings.length < numReadings) {
+			readings.push(value);
+		} else {
+			
+			for (var i = 0; i < (numReadings-1); i++) {
+				if (rangeHigh < readings[i] + buffer) {
+					rangeHigh = readings[i] + buffer;
+				}
+				if (rangeLow > readings[i] - buffer) {
+					rangeLow = readings[i] - buffer;
+				}
+				readings[i] = readings[i+1];
+				
+			}
+			readings[numReadings-1] = value;
+		}
+		if(value > rangeHigh) {
+			checkTap = true;
+		}
+		
+		$scope.messages.push("NoTap: " + value);
+		$scope.apply();
+		}
+		//$scope.messages.push(value);
+    		//$scope.$apply();
     	};
     });
 });
